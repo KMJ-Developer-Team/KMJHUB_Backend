f88029079ab5c20afe4e865934e78a6bd428238c
MoyushAryal <moyusharyal499@gmail.com>
2026-07-23 14:40:30 +0545

    changesss

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializers import PasswordResetSerializer
 # Create your views here.
 
+
+class PasswordResetView(APIView):
+    def post(self, request):
+        serializer = PasswordResetSerializer(data=request.data)
+
+        if serializer.is_valid():
+            email = serializer.validate_data["email"]
+            user = User.objects.filter(email=email).first()
+        return Response(
+            serializer,errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
