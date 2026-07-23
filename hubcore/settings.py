757f1473f7d76fe45e960e310cd1143f09d2fa58
MoyushAryal <moyusharyal499@gmail.com>
2026-07-23 13:47:02 +0545

    profile model base

--- hubcore/settings.py
+++ hubcore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,17 @@ WSGI_APPLICATION = 'hubcore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'kmjhub_db',
+
+        'USER': 'moyush',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
@@ -115,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "accounts.User"
