36a022141b8dd8aeab9f78097e3ae4dbbcb2b658
Jason Acharya <acharyajason12@gmail.com>
2026-07-23 12:31:07 +0545

    Configure PostgreSQL as project database

--- hubcore/settings.py
+++ hubcore/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'hubcore.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "kmjhub_db",
+        "USER": "postgres",
+        "PASSWORD": "admin123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
