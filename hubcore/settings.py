5d4ef86a75732d7c8b7605a748eb662c6305bb07
kushal-upreti <kushalupreti66@gmail.com>
2026-07-24 13:50:52 +0545

    produts app created

--- hubcore/settings.py
+++ hubcore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "apps.accounts",
+    "apps.products",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
 ]
