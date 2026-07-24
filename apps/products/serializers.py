25f3e2627066128ceb6c2279d850e46117832bca
Jason Acharya <acharyajason12@gmail.com>
2026-07-24 22:44:14 +0545

    Completed product-list and product-details endpoints

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework import serializers
-from .models import Product, Category
+from .models import Product
 
 
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'slug', 'price', 'product_type', 'category', 'created_at']
+        fields = [
+            'id', 
+            'name',
+            'slug',
+            'price',
+            'product_type',
+            'category',
+            'created_at',
+        ]
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'name',
+            'slug',
+            'price',
+            'product_type',
+            'category',
+            'created_at',
+            'description'
+        ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,17 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product, Category
-from .serializers import ProductListSerializer
+from .models import Product
 from rest_framework import permissions
+from .serializers import ProductListSerializer, ProductDetailSerializer
 
 
 class ProductListAPIView(APIView):
     permission_classes = [permissions.AllowAny]
+
     def get(self, request):
         products = Product.objects.filter(is_active=True).order_by("-created_at")
         serializer = ProductListSerializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-        
 
+
+class ProductDetailAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, slug):
+        try:
+            product = Product.objects.get(slug=slug, is_active=True)
+        except Product.DoesNotExist:
+            return Response({"error": "Product not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ProductDetailSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
